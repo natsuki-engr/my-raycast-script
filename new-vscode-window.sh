fa9cfe1ec455074c061ba651b89b4ff87b29dab2
natsuki ueda <biafrajr@gmail.com>
2025-05-11 10:13:20 +0900

    vscode new window

---
+++ new-vscode-window.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title New VSCode Window
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📂
+
+# Documentation:
+# @raycast.author biafrajr
+# @raycast.authorURL https://raycast.com/biafrajr
+
+code --new-window
