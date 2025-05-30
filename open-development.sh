5f41bff10de3c9903d45d381a1a74d6b2508aeb6
natsuki ueda <biafrajr@gmail.com>
2025-05-30 18:26:17 +0900

    open folders frequently opened

---
+++ open-development.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Development
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 💻
+
+# Documentation:
+# @raycast.description Open the Development folder in Finder
+# @raycast.author Your Name
+# @raycast.authorURL https://raycast.com/yourname
+
+open "$HOME/Development"
+echo ""
---
+++ open-downloads.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Downloads
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📂
+
+# Documentation:
+# @raycast.description Open the Downloads folder in Finder
+# @raycast.author Your Name
+# @raycast.authorURL https://raycast.com/yourname
+
+open "$HOME/Downloads"
+echo ""
