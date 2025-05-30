48051ed5ea8b1144d9f7095676c6c3793297cf03
natsuki ueda <biafrajr@gmail.com>
2025-05-30 18:28:58 +0900

    open google calendar

---
+++ open-google-calendar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Google Calendar
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📅
+
+# Documentation:
+# @raycast.author natsuki
+# @raycast.authorURL https://raycast.com/natsuki
+
+open -a "Google Chrome" "https://calendar.google.com"
+echo ""
