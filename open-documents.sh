22ef09f24dff20191422fdbe0d3e39fc71dc8c22
natsuki ueda <biafrajr@gmail.com>
2025-06-29 12:49:44 +0900

    Add scripts to open Downloads, Development, and Documents folders in Finder

---
+++ open-documents.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Documents
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🗂️
+
+# Documentation:
+# @raycast.description Open the Documents folder in Finder
+# @raycast.author Your Name
+# @raycast.authorURL https://raycast.com/yourname
+
+open "$HOME/Documents"
+echo ""
